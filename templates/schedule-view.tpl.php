4abe85ef9783527a6326fedde5299c3f6cce2228
Jeremy Smith <goodanswerdesign@gmail.com>
2015-08-02 00:36:36 -0600

    Schedule template now displays properly

--- templates/schedule-view.tpl.php
+++ templates/schedule-view.tpl.php
@@ -1,23 +1,23 @@
 <div id="tabs">
 <ul>
-<?php foreach ($groups as $key => $value) : ?>
-<li><a href="#<?php print $key; ?>">
-    <?php print $key; ?>
+<?php foreach ($groups as $group) : ?>
+<li><a href="#<?php print $group['name']; ?>">
+    <?php print $group['name']; ?>
     <br><span class="group-date">
-        <?php print $value['date']['start_date']; ?> - <?php print $value['date']['end_date']; ?>
+        <?php print $group['date']['start_date']; ?> - <?php print $group['date']['end_date']; ?>
     </span>
     </a></li>
 <?php endforeach; ?>
 </ul>
-<?php foreach ($groups as $key => $value) : ?>
-<div id="<?php print $key; ?>">
-<?php foreach ($entries[$key] as $node) : ?>
+<?php foreach ($groups as $group) : ?>
+<div id="<?php print $group['name']; ?>">
+<?php foreach ($group['entries'] as $node) : ?>
 <div class="schedule-item">
-<div class="schedule-item-content <?php print $node['node']->title; ?>">
-        <h1><?php print $node['node']->title; ?></h1>
+<div class="schedule-item-content <?php print $node->title; ?>">
+        <h1><?php print $node->title; ?></h1>
         <span class="entry-date-range">
-        <?php print $node['node']->lhe_start_date['und'][0]['value']; ?> - 
-        <?php print $node['node']->lhe_end_date['und'][0]['value']; ?> 
+        <?php print $node->lhe_start_date['und'][0]['value']; ?> - 
+        <?php print $node->lhe_end_date['und'][0]['value']; ?> 
         </span>
  
         <table class="library-hours-table">
@@ -30,71 +30,71 @@
             <tbody>
                 <tr>
                     <td class="day">Monday</td>
-                    <?php if (strtolower($node['node']->lhe_monday_hours_open['und'][0]['value']) != 'closed') : ?>
-                    <td class="hours"><?php print $node['node']->lhe_monday_hours_open['und'][0]['value']; ?> - 
-                    <?php print $node['node']->lhe_monday_hours_close['und'][0]['value']; ?></td>
+                    <?php if (strtolower($node->lhe_monday_hours_open['und'][0]['value']) != 'closed') : ?>
+                    <td class="hours"><?php print $node->lhe_monday_hours_open['und'][0]['value']; ?> - 
+                    <?php print $node->lhe_monday_hours_close['und'][0]['value']; ?></td>
                     <?php endif; ?>
-                    <?php if (strtolower($node['node']->lhe_monday_hours_open['und'][0]['value']) == 'closed') : ?>
+                    <?php if (strtolower($node->lhe_monday_hours_open['und'][0]['value']) == 'closed') : ?>
                     <td class="hours">Closed</td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td class="day">Tuesday</td>
-                    <?php if (strtolower($node['node']->lhe_tuesday_hours_open['und'][0]['value']) != 'closed') : ?>
-                    <td class="hours"><?php print $node['node']->lhe_tuesday_hours_open['und'][0]['value']; ?> - 
-                    <?php print $node['node']->lhe_tuesday_hours_close['und'][0]['value']; ?></td>
+                    <?php if (strtolower($node->lhe_tuesday_hours_open['und'][0]['value']) != 'closed') : ?>
+                    <td class="hours"><?php print $node->lhe_tuesday_hours_open['und'][0]['value']; ?> - 
+                    <?php print $node->lhe_tuesday_hours_close['und'][0]['value']; ?></td>
                     <?php endif; ?>
-                    <?php if (strtolower($node['node']->lhe_tuesday_hours_open['und'][0]['value']) == 'closed') : ?>
+                    <?php if (strtolower($node->lhe_tuesday_hours_open['und'][0]['value']) == 'closed') : ?>
                     <td class="hours">Closed</td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td class="day">Wednesday</td>
-                    <?php if (strtolower($node['node']->lhe_wednesday_hours_open['und'][0]['value']) != 'closed') : ?>
-                    <td class="hours"><?php print $node['node']->lhe_wednesday_hours_open['und'][0]['value']; ?> - 
-                    <?php print $node['node']->lhe_wednesday_hours_close['und'][0]['value']; ?></td>
+                    <?php if (strtolower($node->lhe_wednesday_hours_open['und'][0]['value']) != 'closed') : ?>
+                    <td class="hours"><?php print $node->lhe_wednesday_hours_open['und'][0]['value']; ?> - 
+                    <?php print $node->lhe_wednesday_hours_close['und'][0]['value']; ?></td>
                     <?php endif; ?>
-                    <?php if (strtolower($node['node']->lhe_wednesday_hours_open['und'][0]['value']) == 'closed') : ?>
+                    <?php if (strtolower($node->lhe_wednesday_hours_open['und'][0]['value']) == 'closed') : ?>
                     <td class="hours">Closed</td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td class="day">Thursday</td>
-                    <?php if (strtolower($node['node']->lhe_thursday_hours_open['und'][0]['value']) != 'closed') : ?>
-                    <td class="hours"><?php print $node['node']->lhe_thursday_hours_open['und'][0]['value']; ?> - 
-                    <?php print $node['node']->lhe_thursday_hours_close['und'][0]['value']; ?></td>
+                    <?php if (strtolower($node->lhe_thursday_hours_open['und'][0]['value']) != 'closed') : ?>
+                    <td class="hours"><?php print $node->lhe_thursday_hours_open['und'][0]['value']; ?> - 
+                    <?php print $node->lhe_thursday_hours_close['und'][0]['value']; ?></td>
                     <?php endif; ?>
-                    <?php if (strtolower($node['node']->lhe_thursday_hours_open['und'][0]['value']) == 'closed') : ?>
+                    <?php if (strtolower($node->lhe_thursday_hours_open['und'][0]['value']) == 'closed') : ?>
                     <td class="hours">Closed</td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td class="day">Friday</td>
-                    <?php if (strtolower($node['node']->lhe_friday_hours_open['und'][0]['value']) != 'closed') : ?>
-                    <td class="hours"><?php print $node['node']->lhe_friday_hours_open['und'][0]['value']; ?> - 
-                    <?php print $node['node']->lhe_friday_hours_close['und'][0]['value']; ?></td>
+                    <?php if (strtolower($node->lhe_friday_hours_open['und'][0]['value']) != 'closed') : ?>
+                    <td class="hours"><?php print $node->lhe_friday_hours_open['und'][0]['value']; ?> - 
+                    <?php print $node->lhe_friday_hours_close['und'][0]['value']; ?></td>
                     <?php endif; ?>
-                    <?php if (strtolower($node['node']->lhe_friday_hours_open['und'][0]['value']) == 'closed') : ?>
+                    <?php if (strtolower($node->lhe_friday_hours_open['und'][0]['value']) == 'closed') : ?>
                     <td class="hours">Closed</td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td class="day">Saturday</td>
-                    <?php if (strtolower($node['node']->lhe_saturday_hours_open['und'][0]['value']) != 'closed') : ?>
-                    <td class="hours"><?php print $node['node']->lhe_saturday_hours_open['und'][0]['value']; ?> - 
-                    <?php print $node['node']->lhe_saturday_hours_close['und'][0]['value']; ?></td>
+                    <?php if (strtolower($node->lhe_saturday_hours_open['und'][0]['value']) != 'closed') : ?>
+                    <td class="hours"><?php print $node->lhe_saturday_hours_open['und'][0]['value']; ?> - 
+                    <?php print $node->lhe_saturday_hours_close['und'][0]['value']; ?></td>
                     <?php endif; ?>
-                    <?php if (strtolower($node['node']->lhe_saturday_hours_open['und'][0]['value']) == 'closed') : ?>
+                    <?php if (strtolower($node->lhe_saturday_hours_open['und'][0]['value']) == 'closed') : ?>
                     <td class="hours">Closed</td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td class="day">Sunday</td>
-                    <?php if (strtolower($node['node']->lhe_sunday_hours_open['und'][0]['value']) != 'closed') : ?>
-                    <td class="hours"><?php print $node['node']->lhe_sunday_hours_open['und'][0]['value']; ?> - 
-                    <?php print $node['node']->lhe_sunday_hours_close['und'][0]['value']; ?></td>
+                    <?php if (strtolower($node->lhe_sunday_hours_open['und'][0]['value']) != 'closed') : ?>
+                    <td class="hours"><?php print $node->lhe_sunday_hours_open['und'][0]['value']; ?> - 
+                    <?php print $node->lhe_sunday_hours_close['und'][0]['value']; ?></td>
                     <?php endif; ?>
-                    <?php if (strtolower($node['node']->lhe_sunday_hours_open['und'][0]['value']) == 'closed') : ?>
+                    <?php if (strtolower($node->lhe_sunday_hours_open['und'][0]['value']) == 'closed') : ?>
                     <td class="hours">Closed</td>
                     <?php endif; ?>
                 </tr>
@@ -104,18 +104,18 @@
 </div> <!--    End of Schedule Item-->
 
 <?php endforeach; ?>
-<?php if (isset($exceptions[$key])) : ?>
+<?php if (isset($group['exceptions'])) : ?>
 <div class="exception-container">
 <div class="exceptions">
 <h1>Exceptions</h1>
 
-<?php foreach ($exceptions[$key] as $node) : ?>
-        <p><?php print $node['node']->lhe_exception_start_date['und'][0]['value']; ?> to  
-        <?php print $node['node']->lhe_exception_end_date['und'][0]['value']; ?> - Closed for <?php print $node['node']->title; ?></p>
+<?php foreach ($group['exceptions'] as $node) : ?>
+        <p><?php print $node->lhe_exception_start_date['und'][0]['value']; ?> to  
+        <?php print $node->lhe_exception_end_date['und'][0]['value']; ?> - Closed for <?php print $node->title; ?></p>
 <?php endforeach; ?>
 </div>
+</div><!--    End of exception-container-->
 <?php endif; ?>
 </div>
-</div><!--    End of exception-container-->
 <?php endforeach; ?>
 </div>
